7e87244c43158f0f61ec108c1afe11f116f88e8f
Your Name <jennygzma@gmail.com>
2026-03-08 15:54:57 -0400

    save

--- apps/web/src/theme/approvedIcons.ts
+++ apps/web/src/theme/approvedIcons.ts
@@ -1,12 +1,16 @@
+import BarChartRounded from "@mui/icons-material/BarChartRounded";
 import PauseRounded from "@mui/icons-material/PauseRounded";
 import PlayArrowRounded from "@mui/icons-material/PlayArrowRounded";
 import ReplayRounded from "@mui/icons-material/ReplayRounded";
+import SettingsRounded from "@mui/icons-material/SettingsRounded";
 
 // Central approved icon registry. New icons must be added here only after user approval.
 export const approvedIcons = {
   pause: PauseRounded,
   play: PlayArrowRounded,
-  replay: ReplayRounded
+  replay: ReplayRounded,
+  settings: SettingsRounded,
+  stats: BarChartRounded
 } as const;
 
 export const approvedIconNames = Object.keys(approvedIcons) as Array<keyof typeof approvedIcons>;
